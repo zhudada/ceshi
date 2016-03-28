f1b7cdb5bf9b12024ef3db4b69b8a611ceee4952
zhujun <zhujun@kua-jie.com>
2016-03-28 09:42:10 +0800

    Initial Commit

---
+++ 地方法规·/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  地方法规·
+//
+//  Created by kuajie on 16/3/28.
+//  Copyright © 2016年 kuajie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 地方法规·/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  地方法规·
+//
+//  Created by kuajie on 16/3/28.
+//  Copyright © 2016年 kuajie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
